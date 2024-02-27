4dc858fe5db9fe7e8641458dde74d314db29fed7
bourzat <bourzatilyas@gmail.com>
2024-02-27 19:06:18 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import time
+from turtle import Screen
+
+from ball import Ball
+from paddle import Paddle
+from scoreboard import ScoreBoard
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+score = ScoreBoard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+        score.update_scoreboard()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.r_point()
+        score.update_scoreboard()
+
+screen.exitonclick()
